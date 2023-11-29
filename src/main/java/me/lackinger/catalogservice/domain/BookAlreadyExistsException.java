22f16920583559dda2c245317883afdac05e0a65
Ralf Lackinger <ralf_lackinger@yahoo.de>
2023-11-29 12:15:15 +0100

    add chapter 3 content

---
+++ src/main/java/me/lackinger/catalogservice/domain/BookAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package me.lackinger.catalogservice.domain;
+
+public class BookAlreadyExistsException extends RuntimeException {
+
+    public BookAlreadyExistsException(String isbn) {
+        super("A book with ISBN " + isbn + " already exists.");
+    }
+
+}
---
+++ src/main/java/me/lackinger/catalogservice/domain/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package me.lackinger.catalogservice.domain;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(String isbn) {
+        super("The book with ISBN " + isbn + " was not found.");
+    }
+
+}
