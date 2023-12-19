d0b96b5e23451cfceef6a6ab034e70c40ffe0e80
Ralf Lackinger <ralf_lackinger@yahoo.de>
2023-12-19 12:45:02 +0100

    add postgres

---
+++ src/main/java/me/lackinger/catalogservice/config/DataConfig.java
@@ -0,0 +1,9 @@
+package me.lackinger.catalogservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
+
+@Configuration
+@EnableJdbcAuditing
+public class DataConfig {
+}
